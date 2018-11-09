e80cbb9b2e7dfab84cd5cb98a11cefaf1ab4f221
txfang <tianxf940115@163.com>
2018-11-09 10:22:56 +0800

    修改

--- SpringBootKafkaDemo/src/main/java/com/xiaour/spring/boot/kafka/KafkaApplication.java
+++ SpringBootKafkaDemo/src/main/java/com/xiaour/spring/boot/kafka/KafkaApplication.java
@@ -3,6 +3,7 @@ package com.xiaour.spring.boot.kafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//田雄方
 @SpringBootApplication
 public class KafkaApplication {
 
